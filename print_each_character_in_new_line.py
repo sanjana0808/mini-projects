2617d7d4e406fc065aa58a09026beb1cde636700
Sanjana <sanjanaakapoor6@gmail.com>
2022-07-15 10:25:57 +0530

    Day 10, completed hangman game

---
+++ print_each_character_in_new_line.py
@@ -0,0 +1,14 @@
+def letter(s):
+    n = len(s)
+    i = 0
+    while i < n:
+        print(s[i])
+        i = i+1
+# 1st step - call the function
+# 2nd step - the computer will assign the length to n
+# 3rd step - the index will start from 0 which is stored in i
+# 4th step - the computer will enter in while loop when i is less than n and will remain in the\
+# loop till the condition is true
+# 5th step - it will print the value of s at i
+# 6th step - the value of i will increase by 1 to print all the letters one by one in each line
+print(letter("sanjana"))
